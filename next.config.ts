99abc4f7eecab0d95b7255daaf8a5ee6c340c65a
borren <ezequielgiribarren@gmail.com>
2025-07-24 16:20:30 -0300

    a

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,24 @@ const nextConfig: NextConfig = {
         port: '3000',
         pathname: '/api/media/file/**',
       },
+      {
+        protocol: 'http',
+        hostname: '69.62.110.55',
+        port: '3000',
+        pathname: '/api/media/file/**',
+      },
     ],
   },
   env: {
     PAYLOAD_API_URL: process.env.PAYLOAD_API_URL || 'http://localhost:3000/api',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.PAYLOAD_API_URL}/:path*`,
+      },
+    ];
   },
 };
 
